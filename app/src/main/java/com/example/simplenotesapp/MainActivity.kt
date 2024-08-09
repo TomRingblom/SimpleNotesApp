9a551eb07a2ca5f27b0ef2cdc5c630e12cbc02f2
TomRingblom <t_ringblom@hotmail.com>
2024-08-09 23:06:35 +0200

    Add icon to alert dialog.

--- app/src/main/java/com/example/simplenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenotesapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -110,7 +111,10 @@ fun SimpleNotesApp() {
         when {
             openAlertDialog.value -> {
                 AlertDialog(
-                    title = { Text(text = "Delete Note") },
+                    icon = { Icon(Icons.Filled.Warning, contentDescription = null)},
+                    title = {
+                        Text(text = "Delete Note")
+                    },
                     text = {
                         Text(text = "Are you sure you want to delete this note?")
                     },
