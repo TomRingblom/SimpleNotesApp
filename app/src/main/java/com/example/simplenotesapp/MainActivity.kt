4cd112e1a4bcf864518054624e97458275ee81a7
TomRingblom <t_ringblom@hotmail.com>
2024-08-10 11:55:39 +0200

    Add keyboard options and actions.

--- app/src/main/java/com/example/simplenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenotesapp/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Warning
@@ -29,7 +31,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -93,13 +97,27 @@ fun SimpleNotesApp() {
                 value = text,
                 onValueChange = { text = it},
                 label = { Text("Add a note")},
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    capitalization = KeyboardCapitalization.Sentences,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        if(text.isNotEmpty()) {
+                            addNoteToViewModel(viewModel, text)
+                            text = ""
+                        }
+                    }
+                ),
                 modifier = Modifier.fillMaxWidth()
             )
 
             Button(
                 onClick = {
                     if(text.isNotEmpty()) {
-                        viewModel.addNote(text)
+                        addNoteToViewModel(viewModel, text)
                         text = ""
                     }
                 }
@@ -138,6 +156,10 @@ fun SimpleNotesApp() {
             }
         }
     }
+}
+
+private fun addNoteToViewModel(viewModel: NotesViewModel, note: String) {
+    viewModel.addNote(note)
 }
 
 //@Composable
