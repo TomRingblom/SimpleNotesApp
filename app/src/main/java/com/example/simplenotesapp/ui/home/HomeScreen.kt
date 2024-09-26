d764947f04e537de9edd6175115b559444e3f805
TomRingblom <t_ringblom@hotmail.com>
2024-09-26 20:02:59 +0200

    Add random background color to NoteItem.

--- app/src/main/java/com/example/simplenotesapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/simplenotesapp/ui/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.simplenotesapp.ui.home
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,20 +34,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.simplenotesapp.NotesViewModel
 import com.example.simplenotesapp.R
+import com.example.simplenotesapp.SimpleNotesApp
 import com.example.simplenotesapp.data.Note
+import com.example.simplenotesapp.data.NoteRepository
 import com.example.simplenotesapp.navigation.Screen
 import com.example.simplenotesapp.ui.AppViewModelProvider
+import com.example.simplenotesapp.ui.theme.SimpleNotesAppTheme
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 @Composable
 fun HomeScreen(viewModel: NotesViewModel = viewModel(factory = AppViewModelProvider.Factory), navController: NavHostController) {
@@ -89,36 +96,68 @@ fun NoteList(
         modifier = modifier
             .padding(32.dp)
     ) {
+        val colors = listOf(0xFFFFDADB, 0xFFD1EAED, 0xFFFFD4AA, 0xFFFDF3B4)
         notes.forEach { note ->
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(
-                    text = note.text,
-                    modifier = Modifier.weight(1f),
-                    fontSize = 24.sp
-                )
-                Icon(
-                    Icons.Filled.Edit,
-                    contentDescription = stringResource(R.string.edit),
-                    modifier = Modifier
-                        .clickable {
-                            Log.i("NoteList", "noteId: ${note.id}")
-                            navController.navigate(route = "${Screen.Edit.name}/${note.id}")
-                        }
-                )
-                Spacer(modifier = Modifier.padding(10.dp))
-                Icon(
-                    Icons.Filled.Clear,
-                    contentDescription = stringResource(R.string.remove),
-                    modifier = Modifier
-                        .clickable {
-                            viewModel.noteToDelete = note.id
-                            viewModel.updateAlertDialog(true)
-                        }
-                )
-            }
+            val randomNumber = Random.nextInt(colors.size)
+            NoteItem(
+                note = note,
+                viewModel = viewModel,
+                navController = navController,
+                color = colors[randomNumber]
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
         }
     }
 }
+
+@Composable
+fun NoteItem(
+    note: Note,
+    viewModel: NotesViewModel,
+    navController: NavHostController,
+    color: Long
+) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color(color))
+        .padding(8.dp)
+    ) {
+        Text(
+            text = note.text,
+            modifier = Modifier.weight(1f),
+            fontSize = 24.sp
+        )
+        Icon(
+            Icons.Filled.Edit,
+            contentDescription = stringResource(R.string.edit),
+            modifier = Modifier
+                .clickable {
+                    Log.i("NoteList", "noteId: ${note.id}")
+                    navController.navigate(route = "${Screen.Edit.name}/${note.id}")
+                }
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Icon(
+            Icons.Filled.Clear,
+            contentDescription = stringResource(R.string.remove),
+            modifier = Modifier
+                .clickable {
+                    viewModel.noteToDelete = note.id
+                    viewModel.updateAlertDialog(true)
+                }
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun NoteItemPreview() {
+//    NoteItem(
+//        note = Note(text = "This is a note"),
+//        navController = NavHostController(LocalContext.current),
+//        0xFFD1EAED
+//    )
+//}
 
 @Composable
 fun NoteAlertDialog(viewModel: NotesViewModel) {
