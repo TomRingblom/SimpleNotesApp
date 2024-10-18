9fc8439629ca7309cb03a529528e94ad404982f9
TomRingblom <t_ringblom@hotmail.com>
2024-10-19 00:19:37 +0200

    Add NoteTrashScreen.

---
+++ app/src/main/java/com/example/simplenotesapp/ui/note/NoteTrashScreen.kt
@@ -0,0 +1,10 @@
+package com.example.simplenotesapp.ui.note
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NoteTrashScreen(modifier: Modifier = Modifier) {
+    Text(text = "Trash", modifier = modifier)
+}
